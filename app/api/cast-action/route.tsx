86149499f9f957906810e5b53747b9d646c176e5
madaXc <47150624+hutaky@users.noreply.github.com>
2024-10-09 00:03:21 +0200

    Update route.tsx

--- app/api/cast-action/route.tsx
+++ app/api/cast-action/route.tsx
@@ -1,4 +1,4 @@
-// app/api/cast-action/route.ts
+https://moxie-earned-frame.vercel.app/app/api/cast-action/route.ts
 import { NextRequest } from "next/server";
 import { appURL } from "../../utils";
 import { frames } from "../../frames/frames";
